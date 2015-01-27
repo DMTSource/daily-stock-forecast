adc3d3779f29cdda74e2143315843cce700953a7
Derek M Tishler <lstrdean@gmail.com>
2015-01-27 03:31:05 -0500

    Update ClearDS.py and image pos error on old index

--- daily-stock-forecast-gae/cleards.py
+++ daily-stock-forecast-gae/cleards.py
@@ -1,17 +1,19 @@
 
 import webapp2
 from google.appengine.ext import ndb
+import numpy as np
 
 from lib.Forecast import Forecast
 
 class MainPage (webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        
-        list_of_entities = Forecast.query()#Forecast.rank < 6000)
-        list_of_keys = ndb.put_multi(list_of_entities)
-        list_of_entities = ndb.get_multi(list_of_keys)
-        ndb.delete_multi(list_of_keys)
+        #print np.arange(1,10001,1000)
+        for i in np.arange(1,10001,1000):
+            list_of_entities = Forecast.query(Forecast.rank < i)#Forecast.rank < 6000)
+            list_of_keys = ndb.put_multi(list_of_entities)
+            list_of_entities = ndb.get_multi(list_of_keys)
+            ndb.delete_multi(list_of_keys)
 
         self.response.out.write('All Forecasts Deleted')
 
--- daily-stock-forecast-gae/dailystockquant.py
+++ daily-stock-forecast-gae/dailystockquant.py
@@ -159,7 +159,7 @@ class MainPage(webapp2.RequestHandler):
             'url_linktext': url_linktext,
         }
 
-        template = JINJA_ENVIRONMENT.get_template('index.html')
+        template = JINJA_ENVIRONMENT.get_template('indexOld.html')
         self.response.write(template.render(template_values))
 
 
