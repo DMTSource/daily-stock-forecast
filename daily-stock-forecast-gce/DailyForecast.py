Fixed scaling and domain offset for history

daily-stock-forecast-gce/DailyForecast.py
@@ -93,8 +93,8 @@ if __name__ == "__main__":
     logging.info("Predicting %s\n"%dayToPredict.date())
     
     NPredPast             = 10
-    history_len           = 50 #days
-    saftey_days           = 5
+    history_len           = 100 #days
+    saftey_days           = 10
 
     startOfPredictSim     = dayToPredict - BDay(NPredPast)
 
@@ -134,10 +134,11 @@ if __name__ == "__main__":
                                            priceFilterHigh=1e6,
                                            minVolume=1000.0,
                                            useThreading=True,
-                                           requiredDomain=history_len+2)
+                                           requiredDomain=history_len+1,
+                                           verbose=True)
 
     #We need to fetch extra days so we have the right # to handle the fixed dx indexing
-    if len(dates[0]) != history_len+2:
+    if len(dates[0]) != history_len+1:
         print "Insufficient domain, increase saftey_days."
         print len(dates[0])
         exit()
@@ -302,13 +303,6 @@ if __name__ == "__main__":
     for i in sortedRankIndexOriginal:
         rank[symbols[i]] = counter
         counter += 1
-    """rank = {}
-    counter = 1
-    for i in rankIndex:
-        rank[symbols[i]] = counter
-        #if counter <= 10:
-        #    print symbols[i], counter, abs((np.array(savedPrediction[symbols[i]])[:,CLOSE][-1] - closePrice[i][-1])/abs(closePrice[i][-1])*100.0)
-        counter += 1"""
     
     if platform.system() != 'Windows':
         # Set the dataset from the command line parameters.
@@ -523,8 +517,8 @@ if __name__ == "__main__":
         for i in np.arange(len(symbols)):
 
             
-            labels = ["High","Low","Open","Close"]
-            colors = ["r","g","b","c"]
+            labels = ["High","Low","Open","Close", "Volume"]
+            colors = ["r","g","b","c", "o"]
             
             fig = plt.figure()
             #plt.subplots_adjust(left=0.12, bottom=0.06, right=0.90, top=0.96, wspace=0.20, hspace=0.08)

daily-stock-forecast-gce/GetHistoricalFromYahoo.py
@@ -28,7 +28,7 @@ def GetStockDataWithThreading(symbol, index, d1, d2, quotes, symbols, names, ful
 
 
 
-def GetHistoricalFromYahoo(fullSymbols, fullNames, fullExchange, fullSector, fullIndustry, date1, date2, priceFilterLow=0.0, priceFilterHigh=1e9, minVolume=0.0, useThreading=False, requiredDomain=1):
+def GetHistoricalFromYahoo(fullSymbols, fullNames, fullExchange, fullSector, fullIndustry, date1, date2, priceFilterLow=0.0, priceFilterHigh=1e9, minVolume=0.0, useThreading=False, requiredDomain=1, verbose=False):
     """
     Download historical daily data from yahoo finance
   
@@ -74,7 +74,8 @@ def GetHistoricalFromYahoo(fullSymbols, fullNames, fullExchange, fullSector, ful
     badDataIndx = np.empty((len(fullSymbols),), dtype=int)
     threads     = []
 
-    print "Starting historical download."
+    if verbose:
+        print "Starting historical download."
 
 
     numCPU = 64.0
@@ -82,7 +83,8 @@ def GetHistoricalFromYahoo(fullSymbols, fullNames, fullExchange, fullSector, ful
     while((len(fullSymbols)-offset)%numCPU != 0.0):
         offset += 1
     numberOfLoops = (len(fullSymbols)-offset)/numCPU
-    print "%d blocks with %d threads\n"%(numCPU, numberOfLoops)
+    if verbose:
+        print "%d blocks with %d threads\n"%(numCPU, numberOfLoops)
 
     if(numCPU==1):
         useThreading = False
@@ -144,7 +146,8 @@ def GetHistoricalFromYahoo(fullSymbols, fullNames, fullExchange, fullSector, ful
     exchanges = np.delete(exchanges,badDataIndx)
     sectors = np.delete(sectors,badDataIndx)
     industries = np.delete(industries,badDataIndx)
-    print "Failed to download %d of %d symbols"%(len(badDataIndx), len(fullSymbols))
+    if verbose:
+        print "Failed to download %d of %d symbols"%(len(badDataIndx), len(fullSymbols))
     
 
     #Covert to vertical array(ease of use for numpy and machine learning)
@@ -153,8 +156,9 @@ def GetHistoricalFromYahoo(fullSymbols, fullNames, fullExchange, fullSector, ful
     exchanges  = np.array(exchanges).T
     sectors  = np.array(sectors).T
     industries  = np.array(industries).T
-    
-    print "Completing historical download."
+
+    if verbose:
+        print "Completing historical download."
 
     #remove none types
     counterBadDim1 = 0
@@ -175,7 +179,8 @@ def GetHistoricalFromYahoo(fullSymbols, fullNames, fullExchange, fullSector, ful
         sectors = np.delete(sectors,badIndex)
         industries = np.delete(industries,badIndex)
     #
-    print "NoneType returned for %d of %d symbols"%(counterBadDim1, len(fullSymbols))
+    if verbose:
+        print "NoneType returned for %d of %d symbols"%(counterBadDim1, len(fullSymbols))
 
     #remove stocks that dont have the full history(non matching dimensions)
     mode = stats.mode(np.array([len(q) for q in quotes]))[0][0]
@@ -198,7 +203,8 @@ def GetHistoricalFromYahoo(fullSymbols, fullNames, fullExchange, fullSector, ful
         sectors = np.delete(sectors,badIndex)
         industries = np.delete(industries,badIndex)
     #
-    print "Bad dimension for additional %d of %d symbol"%(counterBadDim2, len(symbols)+counterBadDim2)
+    if verbose:
+        print "Bad dimension for additional %d of %d symbol"%(counterBadDim2, len(symbols)+counterBadDim2)
 
 
     # check ahead of time for bad values to prevent breaking the fit with bad division of std in next step
@@ -221,7 +227,8 @@ def GetHistoricalFromYahoo(fullSymbols, fullNames, fullExchange, fullSector, ful
         sectors = np.delete(sectors,badIndex)
         industries = np.delete(industries,badIndex)
     #
-    print "INF/NAN error for %d of %d symbol"%(counterBadDim3, len(symbols)+counterBadDim3)
+    if verbose:
+        print "INF/NAN error for %d of %d symbol"%(counterBadDim3, len(symbols)+counterBadDim3)
 
     # Filter the companies by stock price range. Avoid mem error and over plot
     counterBadDim4 = 0
@@ -242,7 +249,8 @@ def GetHistoricalFromYahoo(fullSymbols, fullNames, fullExchange, fullSector, ful
         sectors = np.delete(sectors,badIndex)
         industries = np.delete(industries,badIndex)
     #
-    print "Price range filterd %d of %d symbol"%(counterBadDim4, len(symbols)+counterBadDim4)
+    if verbose:
+        print "Price range filterd %d of %d symbol"%(counterBadDim4, len(symbols)+counterBadDim4)
 
     # Filter the companies by MISSING volume.
     counterBadDim5 = 0
@@ -263,7 +271,8 @@ def GetHistoricalFromYahoo(fullSymbols, fullNames, fullExchange, fullSector, ful
         sectors = np.delete(sectors,badIndex)
         industries = np.delete(industries,badIndex)
     #
-    print "Volume missing filterd %d of %d symbol"%(counterBadDim5, len(symbols)+counterBadDim5)
+    if verbose:
+        print "Volume missing filterd %d of %d symbol"%(counterBadDim5, len(symbols)+counterBadDim5)
 
     # Filter the companies by min mean volume.
     counterBadDim6 = 0
@@ -284,8 +293,9 @@ def GetHistoricalFromYahoo(fullSymbols, fullNames, fullExchange, fullSector, ful
         sectors = np.delete(sectors,badIndex)
         industries = np.delete(industries,badIndex)
     #
-    print "Volume Minimum filterd %d of %d symbol"%(counterBadDim6, len(symbols)+counterBadDim6)
-    print "Stock universe contains %d symbol(s)\n"%(len(symbols))
+    if verbose:
+        print "Volume Minimum filterd %d of %d symbol"%(counterBadDim6, len(symbols)+counterBadDim6)
+        print "Stock universe contains %d symbol(s)\n"%(len(symbols))
 
 
     #Extract needed series for each stock, slice to ensure we get the right domain len
@@ -318,5 +328,6 @@ def GetHistoricalFromYahoo(fullSymbols, fullNames, fullExchange, fullSector, ful
     for q in quotes:
         volume.append(q.volume[-requiredDomain:])
     volume = np.array(volume)
+
 
     return symbols, names, exchanges, sectors, industries, dates, high, low, openPrice, closePrice, volume

daily-stock-forecast-gce/SupportVectorRegression.py
@@ -48,8 +48,23 @@ def SupportVectorRegression(symbol, seriesSet, genPlot=False, returnItems=[], in
 
         """ Data Configuration & Preprocessing """
         # What features does our model have? We can pick from the bar(open, close, high, low, volume)
-        trainingVectors       = np.zeros((series.shape[0]-2, 5),dtype=np.float32)
-        trainingVectors[:, 0] = seriesSet[0][:-2]
+        trainingVectors       = np.zeros((series.shape[0]-1, 5),dtype=np.float32)
+        testSamples           = np.zeros((1, 5), dtype=np.float32)
+        
+        scalers = []
+        count = 0
+        features = [0,1,2,3,4]
+        for i in features:
+            dataToScale               = seriesSet[i]
+            scalers.append(preprocessing.MinMaxScaler(feature_range=(-1, 1)).fit(dataToScale))
+            scaledData                = scalers[count].transform(dataToScale)
+            
+            trainingVectors[:, count] = scaledData[:-1]
+            
+            testSamples[:, count]     = scaledData[-1]
+            count += 1
+
+        """trainingVectors[:, 0] = seriesSet[0][:-2]
         trainingVectors[:, 1] = seriesSet[1][:-2]
         trainingVectors[:, 2] = seriesSet[2][:-2]
         trainingVectors[:, 3] = seriesSet[3][:-2]
@@ -67,16 +82,16 @@ def SupportVectorRegression(symbol, seriesSet, genPlot=False, returnItems=[], in
         trainingVectors[:, 1] = scaler1.transform(trainingVectors[:, 1])
         trainingVectors[:, 2] = scaler2.transform(trainingVectors[:, 2])
         trainingVectors[:, 3] = scaler3.transform(trainingVectors[:, 3])
-        trainingVectors[:, 4] = scaler4.transform(trainingVectors[:, 4])
+        trainingVectors[:, 4] = scaler4.transform(trainingVectors[:, 4])"""
 
         # Target values, we want to use ^ yesterdays bar to predict this day's close price. Use close scaler????????
-        targetValues          = np.zeros((series.shape[0], ),dtype=np.float32)
-        targetValues          = series[1:-1]
+        targetValues          = np.zeros((series.shape[0]-1, ),dtype=np.float32)
+        targetValues[:]       = series[1:]
         scalerTarget          = preprocessing.MinMaxScaler(feature_range=(-1, 1)).fit(targetValues)
-        targetValues          = scalerTarget.transform(targetValues)
+        targetValues[:]       = scalerTarget.transform(targetValues)
                 
         # Test Samples, scaled using the feature training scaler
-        testSamples           = np.zeros((1, 5), dtype=np.float32)
+        """testSamples           = np.zeros((1, 5), dtype=np.float32)
         testSamples[:, 0]     = seriesSet[0][-1]
         testSamples[:, 0]     = scaler0.transform(testSamples[:, 0])
         testSamples[:, 1]     = seriesSet[1][-1]
@@ -86,14 +101,14 @@ def SupportVectorRegression(symbol, seriesSet, genPlot=False, returnItems=[], in
         testSamples[:, 3]     = seriesSet[3][-1]
         testSamples[:, 3]     = scaler3.transform(testSamples[:, 3])
         testSamples[:, 4]     = seriesSet[4][-1]
-        testSamples[:, 4]     = scaler4.transform(testSamples[:, 4])
+        testSamples[:, 4]     = scaler4.transform(testSamples[:, 4])"""
         
         """ Training Weight """
         weight_training = np.power(np.arange(1, targetValues.shape[0]+1,dtype=float), 1)/ \
                           np.power(np.arange(1, targetValues.shape[0]+1,dtype=float), 1).max()
         
         """ Model Optommization """
-        parameters    = {'C':[1, 10, 100], 'gamma': np.logspace(-2, 1, 4)} #'kernel':('linear', 'rbf'),
+        parameters    = {'C':[1, 10, 100], 'gamma': np.logspace(-3, -1, 3)} #'kernel':('linear', 'rbf'),
         SVR_model     = SVR()
         clf           = grid_search.GridSearchCV(SVR_model, parameters)
         clf.fit(trainingVectors, targetValues)